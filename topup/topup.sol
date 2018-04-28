bfae3c3c10e73d11d63906f2ac09f7f2322aeb03
trejgun <trejgun@gmail.com>
2018-04-28 08:59:17 +0800

    fix contract, add abi

--- topup/topup.sol
+++ topup/topup.sol
@@ -1,8 +1,5 @@
 pragma solidity ^0.4.2;
 
-contract BitGuildWhitelist {
-  mapping (address => bool) public whitelist;
-}
 
 contract BitGuildToken {
   function transfer(address _to, uint256 _value) public;
@@ -12,62 +9,30 @@ contract PLATPriceOracle {
   uint256 public PLATprice;
 }
 
-contract Ownable {
-  address public owner;
-
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address newOwner) public onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-}
-
-contract BitGuildTopUp is Ownable {
-  // Token being sold
+contract BitGuildTopUp {
+  // Token contract
   BitGuildToken public token;
 
-  // Whitelist being used
-  BitGuildWhitelist public whitelist;
-
-  // Whitelist being used
+  // Oracle contract
   PLATPriceOracle public oracle;
 
   // Address where funds are collected
   address public wallet;
 
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
   event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount);
 
-  constructor(address _token, address _whitelist, address _oracle, address _wallet) public {
+  constructor(address _token, address _oracle, address _wallet) public {
     require(_token != address(0));
-    require(_whitelist != address(0));
     require(_oracle != address(0));
     require(_wallet != address(0));
 
     token = BitGuildToken(_token);
-    whitelist = BitGuildWhitelist(_whitelist);
     oracle = PLATPriceOracle(_oracle);
     wallet = _wallet;
   }
 
   // low level token purchase function
   function buyTokens() public payable {
-    require(whitelist.whitelist(msg.sender));
-
     // calculate token amount to be created
     uint256 tokens = getTokenAmount(msg.value, oracle.PLATprice());
 
@@ -87,6 +52,6 @@ contract BitGuildTopUp is Ownable {
 
   // fallback function
   function () external payable {
-    revert();
+    buyTokens();
   }
 }
