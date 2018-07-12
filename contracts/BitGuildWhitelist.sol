f6801cf9a7a80c01895a6c201bc964ac6e9abbd8
calvinmd <calvinpak@gmail.com>
2018-07-12 16:58:23 -0700

    Added whitelist

---
+++ contracts/BitGuildWhitelist.sol
@@ -0,0 +1,111 @@
+pragma solidity ^0.4.22;
+
+import "./BitGuildAccessAdmin.sol";
+
+
+/**
+ * @title BitGuildWhitelist
+ * @dev A small smart contract to provide whitelist functionality and storage
+ */
+contract BitGuildWhitelist is BitGuildAccessAdmin {
+    address[] public whitelist;
+
+    mapping (address => bool) public isWhitelisted;
+
+    event AddressWhitelisted(address indexed addr, address operator);
+    event AddressRemovedFromWhitelist(address indexed addr, address operator);
+
+    /// @dev Throws if _address is not in whitelist.
+    modifier onlyWhitelisted(address _address) {
+        require(
+            isWhitelisted[_address],
+            "Address is not on the whitelist."
+        );
+        _;
+    }
+
+    // Doesn't accept eth
+    function () external payable {
+        revert();
+    }
+
+    /**
+     * @dev Allow operators to add whitelisted contracts
+     * @param _newAddr New whitelisted contract address
+     */
+    function addToWhitelist(address _newAddr) public onlyOperator {
+        require(
+            _newAddr != address(0),
+            "Invalid new address."
+        );
+
+        // Make sure no dups
+        require(
+            !isWhitelisted[_newAddr],
+            "Address is already whitelisted."
+        );
+
+        whitelist.push(_newAddr);
+        isWhitelisted[_newAddr] = true;
+
+        emit AddressWhitelisted(_newAddr, msg.sender);
+    }
+
+    /**
+     * @dev Allow operators to remove a contract from the whitelist
+     * @param _addr Contract address to be removed
+     */
+    function removeFromWhitelist(address _addr) public onlyOperator {
+        require(
+            _addr != address(0),
+            "Invalid address."
+        );
+
+        // Make sure the address is in whitelist
+        require(
+            isWhitelisted[_addr],
+            "Address not in whitelist."
+        );
+
+        // Manual array manipulation:
+        // - replace the _operator with last operator in array
+        // - remove the last item from array
+        address lastAddress = whitelist[whitelist.length - 1];
+        for (uint i = 0; i < whitelist.length; i++) {
+            if (whitelist[i] == _addr) {
+                whitelist[i] = lastAddress;
+            }
+        }
+        whitelist.length -= 1; // remove the last element
+
+        isWhitelisted[_addr] = false;
+        emit AddressRemovedFromWhitelist(_addr, msg.sender);
+    }
+
+    /**
+     * @dev Allow operators to update whitelist contracts in bulk
+     * @param _addresses Array of addresses to be processed
+     * @param _whitelisted Boolean value -- to add or remove from whitelist
+     */
+    function whitelistAddress(address[] _addresses, bool _whitelisted) public onlyOperator {
+        for (uint i = 0; i < _addresses.length; i++) {
+            address addr = _addresses[i];
+            if (isWhitelisted[addr] == _whitelisted) continue;
+            if (_whitelisted) {
+                addToWhitelist(addr);
+            } else {
+                removeFromWhitelist(addr);
+            }
+        }
+    }
+
+    /// @dev Get number of addresses in the whitelist
+    function whitelistCount() public view returns(uint count) {
+        return whitelist.length;
+    }
+
+    /// @dev Return all whitelisted addresses
+    function getWhitelist() public onlyOperator view returns(address []) {
+        return whitelist;
+    }
+}
